88dbbbd0d0861ca09ea5f62294c7bd64c6ce6054
Xavier Ang <xaveang@gmail.com>
2025-09-04 02:25:59 +0800

    Only process cwd of (x)terms

--- lib/herbex.ex
+++ lib/herbex.ex
@@ -6,44 +6,58 @@ defmodule Herbex do
 
   defp loop() do
     receive do
-      message -> process_hook(message)
+      message -> process_message(message)
     end
     loop()
   end
 
-  defp process_hook({_port, {:data, data}}) do
-    [hook | _] = String.split(data, "\t")
-
-    case hook do
-      "focus_changed" -> update_cwd()
-      # when "cd" is run in an open terminal, window_title_changed hook is also emitted
-      "window_title_changed" -> update_cwd()
+  defp process_message({_port, {:data, data}}) do
+    case String.split(data, "\t", trim: true) do
+      [hook, winid | _] -> process_hook(hook, winid)
       _ -> nil
     end
   end
 
-  defp update_cwd() do
-    file = System.user_home()
-      |> Path.join(".cwd")
-    case System.cmd("herbstclient", ["get_attr", "clients.focus.pid"])
+  defp process_hook(hook, winid) do
+    case hook do
+      "focus_changed" -> update_cwd(winid)
+      # when "cd" is run in an open terminal, window_title_changed hook is also emitted
+      "window_title_changed" -> update_cwd(winid)
+      _ -> nil
+    end
+  end
+
+  defp update_cwd(winid) do
+    with {wm_class, 0} <- System.cmd("xprop", ["-id", winid, "WM_CLASS"]),
+        [_] <- Regex.run(~r/term/i, wm_class)
     do
-      {parent_pid, 0} ->
-        parent_pid = String.trim(parent_pid)
-        pids = System.cmd("pstree", ["-p", parent_pid])
-          |> elem(0)
-          |> String.split("\n", trim: true)
-          |> List.first()
+      update_cwd()
+    end
+  end
 
-        [pid] = Regex.scan(~r/(\d+)/, pids, capture: :first)
-          |> Enum.at(1)
-        {cwd, 0} = System.cmd("readlink", ["/proc/#{pid}/cwd"])
-        cwd = String.trim(cwd)
+  defp update_cwd() do
+    cwd_file = System.user_home()
+      |> Path.join(".cwd")
 
-        case File.write(file, cwd) do
-          :ok -> IO.puts("#{cwd} written to #{file}")
-          err -> IO.puts(err)
-        end
+    with {parent_pid, 0} <- System.cmd(
+      "herbstclient", ["get_attr", "clients.focus.pid"])
+    do
+      parent_pid = String.trim(parent_pid)
+      pids = System.cmd("pstree", ["-p", parent_pid])
+        |> elem(0)
+        |> String.split("\n", trim: true)
+        |> List.first()
 
+      [pid] = Regex.scan(~r/(\d+)/, pids, capture: :first)
+        |> Enum.at(1)
+      {cwd, 0} = System.cmd("readlink", ["/proc/#{pid}/cwd"])
+      cwd = String.trim(cwd)
+
+      case File.write(cwd_file, cwd) do
+        :ok -> IO.puts("#{cwd} written to #{cwd_file}")
+        err -> IO.puts(err)
+      end
+    else
       _ -> {:error, "Error retrieving PID"}
     end
   end
